50132341f51b506641006a0eff9f6b5ff85c5f49
Khusrav <kroydjaykven@gmail.com>
2024-04-22 16:57:36 +0500

    AF-5

--- src/pages/camera/Camera.js
+++ src/pages/camera/Camera.js
@@ -1,8 +1,19 @@
-import CameraWrapper from './CameraWrapper'
-
-
-const Camera = () => (
-      <CameraWrapper />
-);
+import CameraWrapper from './CameraWrapper';
+import Header from '../../layout/header/index';
+import DataOpenPhoto from './dataOpenPhoto';
+function Camera() {
+  const dataimg = (imageData) => {
+    console.log(imageData, 'Khusrav');
+  };
+  return (
+    <>
+      <Header />
+      <div className="flex justify-center">
+        <CameraWrapper dataimg={dataimg} />
+        <DataOpenPhoto />
+      </div>
+    </>
+  );
+}
 
 export default Camera;
