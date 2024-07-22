72d70cab88f19bcb5a65ea8932b547b9014ee5e2
Khusrav <kroydjaykven@gmail.com>
2024-07-22 20:53:28 +0500

    update

--- src/routes/CameraRoutes.js
+++ src/routes/CameraRoutes.js
@@ -1,6 +1,5 @@
 import { lazy } from 'react';
 
-// project import
 import Loadable from 'components/Loadable';
 // import MinimalLayout from 'layout/MinimalLayout';
 
--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,14 +1,10 @@
 import { lazy } from 'react';
 
-// project import
 import Loadable from 'components/Loadable';
 import MainLayout from 'layout/MainLayout';
 
-// render - dashboard
 const User = Loadable(lazy(() => import('pages/user')));
 // const Access = Loadable(lazy(() => import('pages/access')));
-
-// ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = () => {
   return [
